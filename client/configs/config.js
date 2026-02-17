aa79aa5e8c58bf0d51155f23d5edf7c5a295422b
Strugariu Sergiu <sstrugariu.7@icloud.com>
2026-02-18 00:19:57 +0200

    Update config.js

--- client/configs/config.js
+++ client/configs/config.js
@@ -1,7 +1,29 @@
 export const authorizedUsers = {
-    "378036418984804353": {
+    "378036418984804353": { // Eu
         role: "director",
         name: "Director",
+        servers: {
+            "1197908906542903337": true,
+            "1468716505829998646": false,
+            "1460783529641251012": false
+        },
+        permissions: {
+            CVSubmission: true,
+            Demisie: false,
+            GuildConfig: false,
+            Permission: false,
+            Points: false,
+            PointsHistory: false,
+            Presence: false,
+            SalaryAdjustment: false,
+            SalaryConfig: false,
+            TimeEntry: true,
+            User: false
+        }
+    },
+    "513027762953519106": { // Parker
+        role: "colaborator",
+        name: "Colaborator",
         servers: {
             "1197908906542903337": true,
             "1468716505829998646": true,
@@ -9,19 +31,41 @@ export const authorizedUsers = {
         },
         permissions: {
             CVSubmission: true,
-            Demisie: true,
-            GuildConfig: true,
-            Permission: true,
-            Points: true,
-            PointsHistory: true,
-            Presence: true,
-            SalaryAdjustment: true,
-            SalaryConfig: true,
+            Demisie: false,
+            GuildConfig: false,
+            Permission: false,
+            Points: false,
+            PointsHistory: false,
+            Presence: false,
+            SalaryAdjustment: false,
+            SalaryConfig: false,
             TimeEntry: true,
-            User: true
+            User: false
         }
     },
-    "282459677860691969": {
+    "243647018352771072": { // VooDoo
+        role: "colaborator",
+        name: "Colaborator",
+        servers: {
+            "1197908906542903337": true,
+            "1468716505829998646": true,
+            "1460783529641251012": true
+        },
+        permissions: {
+            CVSubmission: true,
+            Demisie: false,
+            GuildConfig: false,
+            Permission: false,
+            Points: false,
+            PointsHistory: false,
+            Presence: false,
+            SalaryAdjustment: false,
+            SalaryConfig: false,
+            TimeEntry: true,
+            User: false
+        }
+    },
+    "282459677860691969": { // Angel
         role: "colaborator",
         name: "Colaborator",
         servers: {
