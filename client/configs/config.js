17eb50acbf87ffac718a07f3a45f7123f8480b81
Strugariu Sergiu <sstrugariu.7@icloud.com>
2026-02-20 22:49:59 +0200

    Update config.js

--- client/configs/config.js
+++ client/configs/config.js
@@ -1,26 +1,22 @@
 export const authorizedUsers = {
     "378036418984804353": { // Eu
-        role: "director",
-        name: "Director",
+        role: "Asistent Director",
+        name: "Asistent Director",
         servers: {
             "1197908906542903337": true,
-            "1468716505829998646": true,
-            "1460783529641251012": true,
-            "1308522321459544065": true, // Bahamas
-            "1458865556420690229": true, // Frizerie 7/7 (OLD)
         },
         permissions: {
             CVSubmission: true,
-            Demisie: true,
-            GuildConfig: true,
-            Permission: true,
-            Points: true,
-            PointsHistory: true,
-            Presence: true,
-            SalaryAdjustment: true,
-            SalaryConfig: true,
+            Demisie: false,
+            GuildConfig: false,
+            Permission: false,
+            Points: false,
+            PointsHistory: false,
+            Presence: false,
+            SalaryAdjustment: false,
+            SalaryConfig: false,
             TimeEntry: true,
-            User: true
+            User: false
         }
     },
     "309367153063231488": { // SkyPixel
@@ -118,5 +114,86 @@ export const authorizedUsers = {
             TimeEntry: false,
             User: false
         }
-    }
+    },
+
+    "301783270280921088": { // Radu Stefan
+        role: "Asistent Director",
+        name: "Asistent Director",
+        servers: {
+            "1197908906542903337": true,
+        },
+        permissions: {
+            CVSubmission: true,
+            Demisie: false,
+            GuildConfig: false,
+            Permission: false,
+            Points: false,
+            PointsHistory: false,
+            Presence: false,
+            SalaryAdjustment: false,
+            SalaryConfig: false,
+            TimeEntry: true,
+            User: false
+        }
+    },
+    "575703586030223370": { // Mya
+        role: "Asistent Director",
+        name: "Asistent Director",
+        servers: {
+            "1197908906542903337": true,
+        },
+        permissions: {
+            CVSubmission: true,
+            Demisie: false,
+            GuildConfig: false,
+            Permission: false,
+            Points: false,
+            PointsHistory: false,
+            Presence: false,
+            SalaryAdjustment: false,
+            SalaryConfig: false,
+            TimeEntry: true,
+            User: false
+        }
+    },
+    "229164091451768832": { // Marin
+        role: "Asistent Director",
+        name: "Asistent Director",
+        servers: {
+            "1197908906542903337": true,
+        },
+        permissions: {
+            CVSubmission: true,
+            Demisie: false,
+            GuildConfig: false,
+            Permission: false,
+            Points: false,
+            PointsHistory: false,
+            Presence: false,
+            SalaryAdjustment: false,
+            SalaryConfig: false,
+            TimeEntry: true,
+            User: false
+        }
+    },
+    "602098098289901600": { // Zburlici
+        role: "Asistent Director",
+        name: "Director Afacere",
+        servers: {
+            "1197908906542903337": true,
+        },
+        permissions: {
+            CVSubmission: true,
+            Demisie: false,
+            GuildConfig: false,
+            Permission: false,
+            Points: false,
+            PointsHistory: false,
+            Presence: false,
+            SalaryAdjustment: false,
+            SalaryConfig: false,
+            TimeEntry: true,
+            User: false
+        }
+    },
 };
